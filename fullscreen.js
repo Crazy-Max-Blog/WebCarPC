b325088bbf2263944f272df0228e3fd30952301c
Crazy-Max-Blog <Crazy-Nax-2011@yandex.ru>
2024-06-13 10:49:33 +0400

    Add fullscreen mode

---
+++ fullscreen.js
@@ -0,0 +1,53 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+let fullscreen;
+let fsEnter = document.getElementById('fullscr');
+let fsEnterIn = "Войти в полноэкранный режим";
+let fsEnterOut = "Выйти из полноэкранного режима";
+fsEnter.innerHTML = fsEnterIn;
+fsEnter.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!fullscreen) {
+        fullscreen = true;
+        document.documentElement.requestFullscreen();
+        fsEnter.innerHTML = fsEnterOut;
+        } else {
+        fullscreen = false;
+        document.exitFullscreen();
+        fsEnter.innerHTML = fsEnterIn;
+    }
+});
+document.addEventListener("fullscreenchange", function() { 
+    if((window.fullScreen) || (window.innerWidth == screen.width && window.innerHeight == screen.height)) {
+        fullscreen = true;
+        fsEnter.innerHTML = fsEnterOut;
+        } else {
+        fullscreen = false;
+        fsEnter.innerHTML = fsEnterIn;
+    }
+}); 
